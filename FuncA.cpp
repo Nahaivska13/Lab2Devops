842eeb760441867572f4023a58c9908d19c237c8
Nahaivska13 <Nahaivska13.daryna.nahaivska2017@gmail.com>
2024-10-24 21:58:39 +0000

    Fixed FuncA to calculate sum of first 3 terms

--- FuncA.cpp
+++ FuncA.cpp
@@ -4,8 +4,8 @@
 // Реалізація функції для обчислення суми n елементів ряду
 double FuncAClass::FuncA(int n) {
     double sum = 0;
-    for (int i = 0; i < n; ++i) {
-        // Додай формулу відповідно до завдання
+    for (int i = 0; i < 3; ++i) {
+        
         sum += pow(-1, i) * eulerNumber(2 * i) * pow(x, 2 * i) / factorial(2 * i);
 
     }
